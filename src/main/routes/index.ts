14d30c6eee5563a64dc3e05d067b7dcee0eb3585
Obdurat <omicron_rpl@hotmail.com>
2023-08-03 04:18:20 -0300

    feat: Added routes

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { Handler } from "../../presentation";
+import { getIssues, dispatchIssues, removeIssues } from "../../presentation";
 
 export const endpoints: Router = Router();
     
 endpoints.route("/:owner/:repo")
-    .get(Handler)
+    .get(getIssues)
+    .put(dispatchIssues);
 
+endpoints.route("/:owner/:repo/:issue_id")
+    .delete(removeIssues)
